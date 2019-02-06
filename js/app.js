92b608a45432ca2299e1db081cd7683d66b7ecd7
Mary Cimakasky <MaryCimakasky@Marys-MBP.localdomain>
2019-02-06 14:39:45 -0800

    JS rough sketch. No HTML. Live Server not loading properly, I believe it is due to a file tree error.

---
+++ js/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+//Establish that Y/N === Yes/No
+Y.toLowerCase() = YES.toLowerCase(); 
+N.toLowerCase() = NO.toLowerCase(); 
+
+//Ask the User their name, store in console.  
+var affirmedname = prompt('Hello! What is your name?'); 
+console.log('Asked for a name, result:'+name); 
+
+//Y/N question 1
+var answer1 = prompt(name+'this is question 1', 'Yes or No'); 
+//console.log
+//if/else leading to question 2 and 3 respectively 
+
+//Y/N question 2
+var answer1.2 = prompt('this is a follow up question', 'Yes or No'); 
+//console.log
+
+//Y/N question 3
+var answer 1.3 = prompt('this is a follow up question', 'Yes or No'); 
+//comsole.log
+
+//Y/N question 4
+//console.log
+
+//Y/N question 5
+//console.log 
